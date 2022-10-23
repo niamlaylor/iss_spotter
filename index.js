f34c93dcb55bd4c0ebb4a737d677dceddb6c0b1f
Liam Naylor <liam@liamnaylor.com>
2022-10-22 19:47:49 -0700

    Initial commit: Built out callbacks

---
+++ index.js
@@ -0,0 +1,10 @@
+const { fetchMyIp } = require('./iss');
+
+fetchMyIp((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
